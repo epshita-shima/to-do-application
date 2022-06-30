33fed6767cfd8738648853c00f58abe8520609e4
epshita-shima <epshitashima@gmail.com>
2022-06-30 20:15:38 +0600

    complete navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CompleteTask from './components/CompleteTask/CompleteTask';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Todo from './components/Todo/Todo';
+
+
 
 function App() {
   return (
     <div>
-
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/todo' element={<Todo></Todo>}></Route>
+        <Route path='/completetask' element={<CompleteTask></CompleteTask>}></Route>
+        <Route path='/calender' element={<CompleteTask></CompleteTask>}></Route>
+      </Routes>
     </div>
   );
 }
