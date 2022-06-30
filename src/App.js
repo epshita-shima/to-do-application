85c113b8c251dffc0d143a6e81687e7e942a9f0a
epshita-shima <epshitashima@gmail.com>
2022-06-30 20:56:13 +0600

    complete calender

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Calender from './components/Calender/Calender';
 import CompleteTask from './components/CompleteTask/CompleteTask';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
@@ -15,7 +16,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/todo' element={<Todo></Todo>}></Route>
         <Route path='/completetask' element={<CompleteTask></CompleteTask>}></Route>
-        <Route path='/calender' element={<CompleteTask></CompleteTask>}></Route>
+        <Route path='/calender' element={<Calender></Calender>}></Route>
       </Routes>
     </div>
   );
--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 const Calender = () => {
-    return (
-        <div>
+    const { date, setDate } = useState(new Date());
 
+    return (
+        <div class="card w-96 mx-auto bg-base-100 shadow-xl mt-10">
+            <div class="card-body">
+                <DayPicker
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                ></DayPicker>
+
+            </div>
         </div>
     );
 };
